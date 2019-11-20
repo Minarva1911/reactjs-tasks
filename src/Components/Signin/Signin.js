40011cbecad8d12e06232bc4074f26b7cc30c9bb
minarva <minarvamohan@gmail.com>
2019-11-20 14:23:32 +0530

    adding new file

--- src/Components/Signin/Signin.js
+++ src/Components/Signin/Signin.js
@@ -1,34 +1,37 @@
 import React, { Component } from 'react';
 import { breakStatement } from '@babel/types';
 import './Signin.css';
+import images from "../Image1.jpg";
+import { SocialIcon } from 'react-social-icons';
 
 class Signin extends Component {
-    render() {
-        return (
-            <div className="demo1">
-                Signin Form
-            <div>
-                <img src="./Componments/Images/Image1.jpg" alt="valid"/>
-            <form>
-                <input type="text" placeholder="Mobile/Email" minlength="10" size="30" required/>
-                <br/><br/>
-                <input type="text" placeholder="Password" minlength="10" size="30" required/>
-                <br/><br/>
-                <input type="checkbox" required/>Remember me Forgot Password?
-                <br/><br/>
-                <button >Signin</button>
-            </form>
-            <br/>
-            <div>
-                Create a new account
+  render() {
+    return (
+      <center>
+        <div className="demo1">
+          <div className="head">Signin Form</div>
+            <img src={images}/><br/><br/>
+              <form>
+                <input className="mobile" type="text" placeholder="Mobile/Email" minlength="10" size="40" required/><br/><br/>
+                <input className="mobile" type="text" placeholder="Password" minlength="10" size="40" required/><br/><br/>
+                <input className="box1" type="checkbox" required/><label className="para">Remember me</label>
+                <div className="box2">Forgot Password?</div><br/>
+                <div><button className="button">Signin</button></div>
+              </form><br/>
+                <div>Create a new account<div><br/>
+                <div>(or)</div><br/>
                 <div>
-               <a href="/signup">signup</a>
-               </div>
+                  <SocialIcon url="http://twitter.com/jaketrent" />
+                  <SocialIcon url="http://facebook.com/jaketrent" />
+                  <SocialIcon url="http://google.com/jaketrent" />
+                </div>
+                <a href="/signup">signup</a>
             </div>
-            </div>
-            </div>
-        );
-    }
+          </div>
+        </div>
+      </center>
+    );
+  }
 }
 
 export default Signin;
--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -4,27 +4,29 @@ import './Signup.css';
 class Signup extends Component {
     render() {
         return (
+          <center>
             <div className="demo">
-                Signup Form
-                <form> <br/>
-                <input type="text" placeholder="FirstName" minlength="10" size="30" required/>
-                <br/><br/>
-                <input type="text" placeholder="LastName" minlength="10" size="30" required/>
-                <br/><br/>
-                <input type="text" placeholder="minarvamohan@gmail.com" minlength="10" size="30" required/>
-                <br/><br/>
-                <input type="text" placeholder="Plz enter phone number" minlength="10" size="30" required/>
-                <br/><br/>
-                <input type="text" placeholder="Password" minlength="10" size="30" required/>
-                <br/><br/>
-                <input type="text" placeholder="Conform password" minlength="10" size="30" required/>
-                <br/><br/>
-                <input type="checkbox" required/>I agree to terms & privacy policices 
-                <br/><br/>
-                <button >Signup</button>
-            </form>
+               <div className="head">Signup Form</div> 
+                <form> <br />
+                    <input className="mobile" type="text" placeholder="FirstName" minlength="10" size="40" required />
+                    <br /><br />
+                    <input className="mobile" type="text" placeholder="LastName" minlength="10" size="40" required />
+                    <br /><br />
+                    <input className="mobile" type="text" placeholder="minarvamohan@gmail.com" minlength="10" size="40" required />
+                    <br /><br />
+                    <input className="mobile" type="text" placeholder="Plz enter phone number" minlength="10" size="40" required />
+                    <br /><br />
+                    <input className="mobile" type="text" placeholder="Password" minlength="10" size="40" required />
+                    <br /><br />
+                    <input className="mobile" type="text" placeholder="Conform password" minlength="10" size="40" required />
+                    <br /><br />
+                    <input className="para1" type="checkbox" required /><label className="para">I agree to terms & privacy policices</label>
+                <br /><br />
+                <div><button className="button">Signup</button></div>
+                </form>
             </div>
-        
+            </center>
+
         );
     }
 }
